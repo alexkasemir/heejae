922003f92a2c8367664d5a3187beb1c8a94ff424
wTheRockb <forrestwbutler@gmail.com>
2017-09-23 14:40:08 -0400

    workout models

--- backend/workouts/models.py
+++ backend/workouts/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class workout(models.Model):
+	user = models.ForeignKey(User, related_name='workouts')
+	start_ts = models.DateTimeField(auto_now_add=True)
+	end_ts = models.DateTimeField(auto_now_add=True)
+	
