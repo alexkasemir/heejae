5ba76d09b89430c0c5bb13be0e707cd04980f3ed
Alex Kasemir <kasemir.alex@gmail.com>
2018-03-10 19:22:15 -0500

    fixing like speed

--- backend/posts/serializers/post.py
+++ backend/posts/serializers/post.py
@@ -1,33 +1,34 @@
+
+from __future__ import division
 from django.utils import timezone
 
 from rest_framework import serializers
 from posts.models import Post
 
 
-def get_like_count(like_max, time_since):
-    hours = int(time_since // 3600)
-    minutes = int((time_since % 3600) // 60)
+def get_like_count(post):
+    like_max = post.like_max
+    now = timezone.now()
+    created = post.created_ts
+    ten_pm = created.replace(hour=22, minute=0, second=0, microsecond=0)
 
-    if hours >= 48:
+    time_until_ten = (ten_pm - now).total_seconds()
+    time_between = (ten_pm - created).total_seconds()
+    time_since_created = (now - created).total_seconds()
+
+    if time_until_ten < 0:
         return like_max
-    elif hours < 1:
-        if minutes <= 10:
-            return 0
-        if minutes > 10 and minutes < 60:
-            return int((1/60.0) * minutes * (like_max / 3))
-        else:
-            return int(like_max / 3)
     else:
-        to_return = (2/141.0) * like_max * ((minutes / 60.0) - 48) + like_max
-        return int(to_return)
+        return int((time_since_created / time_between) * like_max)
+
+
+
 
 class GetPostSerializer(serializers.ModelSerializer):
     like_count = serializers.SerializerMethodField()
 
     def get_like_count(self, post):
-        now = timezone.now()
-        created = post.created_ts
-        return get_like_count(post.like_max, (now - created).total_seconds())
+        return get_like_count(post)
 
     class Meta:
         model = Post
